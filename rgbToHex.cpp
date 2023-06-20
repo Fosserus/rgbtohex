323288df115057f6d74b44cb82a28483fed82b1f
Nathan P <87007320+Fosserus@users.noreply.github.com>
2023-06-20 16:48:09 -0500

    added GNU license and cleaned up some lines

--- rgbToHex.cpp
+++ rgbToHex.cpp
@@ -1,3 +1,22 @@
+/*
+    ©2023, Nathan Powell
+    This program is free software: you can redistribute it and/or modify 
+    it under the terms of the GNU General Public License as published by 
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful, 
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+    GNU General Public License for more details.
+   
+    You should have received a copy of the GNU General Public License 
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+    
+    Originally used for a Codewars solution, but thought it'd be a good idea
+    to keep this code in case I need it in the future. Hopefully this also helps someone too!
+*/
+
 
 #include <iostream>
 #include <ctype.h>
@@ -35,25 +54,18 @@ string toHex(int n) {
 string rgb(int r, int g, int b) {
 
     string hexCode = "";
-   
-   
+
    hexCode.append(toHex(r));
    hexCode.append(toHex(g));
    hexCode.append(toHex(b));
 
-    cout << hexCode << endl;
-    
-
+    cout << "#" << hexCode << endl; // comment out if using function on its own
     return hexCode;
-   
 }
 
 int main()
 {
-   
-        
-    
-    rgb(-20, 275, 125);
-
+    // usage: rgb(0, 0, 0);
+    return 0;
 }
 
